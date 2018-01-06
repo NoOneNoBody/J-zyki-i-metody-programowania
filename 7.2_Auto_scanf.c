0b79a5d0134f15e3cf62d9c73e83438eb7884e13
ThereIsNoNameSoFar <micwar@interia.pl>
2018-01-06 21:20:58 +0100

    Update 7.2_Auto_scanf.c

--- 7.2_Auto_scanf.c
+++ 7.2_Auto_scanf.c
@@ -1,4 +1,4 @@
-/*Wykona≥ Micha≥ Warzecha I rok Informatyka EAIIB grupa 6b*/
+/*Wykona≈Ç Micha≈Ç Warzecha I rok Informatyka EAIIB grupa 6b*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
