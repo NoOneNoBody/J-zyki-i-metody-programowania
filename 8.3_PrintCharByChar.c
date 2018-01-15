7ec772501807218c83ba3f3a51c602cfa6b92ef5
NoOneNoBody <35255526+NoOneNoBody@users.noreply.github.com>
2018-01-15 20:41:00 +0100

    Update 8.3_PrintCharByChar.c

--- 8.3_PrintCharByChar.c
+++ 8.3_PrintCharByChar.c
@@ -8,7 +8,7 @@ long long copyArgumentAndPrintCharByCharReturningSumOfArguments(void* data, size
   char *table = (char*)malloc(sizeOfData);
   table = (char*)memcpy((char*)table,(char*)data,sizeOfData);
   long long sum = 0;
-  for(int i=0;i<(sizeOfData/sizeof(char))-sizeof(char);++i)
+  for(int i=0;i<(int)((sizeOfData/sizeof(char))-sizeof(char));++i)
   {
     printf("%x\n",table[i]);
     sum += table[i];
@@ -19,6 +19,6 @@ long long copyArgumentAndPrintCharByCharReturningSumOfArguments(void* data, size
 int main(void)
 {
   char text[] = "Hello world!\n";
-  printf("\nSuma: %lld",copyArgumentAndPrintCharByCharReturningSumOfArguments((char*)text,sizeof(text)));
+  printf("\nSuma: %lld\n",copyArgumentAndPrintCharByCharReturningSumOfArguments((char*)text,sizeof(text)));
   return 0;
 }
