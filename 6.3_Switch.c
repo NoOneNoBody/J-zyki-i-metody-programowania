da9164d034015ff78bfcbf8fc7ec588940b19dbe
NoOneNoBody <35255526+NoOneNoBody@users.noreply.github.com>
2018-02-08 16:37:26 +0100

    Update 6.3_Switch.c

--- 6.3_Switch.c
+++ 6.3_Switch.c
@@ -3,36 +3,36 @@
 #include <stdlib.h>
 #include<time.h>
 
-void Losuj(int tablica[])
+void Losuj(int tablica[], int rozmiar)
 {
-  for(int i=0;i<10;++i)
+  for(int i=0;i<rozmiar;++i)
   {
     tablica[i] = rand()%100;
   }
 }
 
-void Wyswietl(int tablica[])
+void Wyswietl(int tablica[], int rozmiar)
 {
-  for(int i=0;i<10;++i)
+  for(int i=0;i<rozmiar;++i)
   {
     printf("%d ",tablica[i]);
   }
   printf("\n");
 }
 
-void Srednia(int tablica[])
+void Srednia(int tablica[], int rozmiar)
 {
   int suma = 0;
-  for(int i=0;i<10;++i)
+  for(int i=0;i<rozmiar;++i)
   {
     suma += tablica[i];
   }
   printf("Srednia liczb: %f\n",(suma/10.0));
 }
 
-void Sortuj(int tablica[],int posortowana[])
+void Sortuj(int tablica[],int posortowana[], int rozmiar)
 {
-  for(int i=0;i<10;++i)
+  for(int i=0;i<rozmiar;++i)
   {
     posortowana[i]=tablica[i];
     int j = i;
@@ -46,11 +46,11 @@ void Sortuj(int tablica[],int posortowana[])
   }
 }
 
-void MinMax(int tablica[])
+void MinMax(int tablica[], int rozmiar)
 {
   int min = tablica[0];
   int max = tablica[0];
-  for(int i=1;i<10;++i)
+  for(int i=1;i<rozmiar;++i)
   {
     if(tablica[i]<min)
     {
@@ -64,7 +64,7 @@ void MinMax(int tablica[])
   printf("Minimalna wartosc to: %d, a maksymalna to: %d.\n",min,max);
 }
 
-void MenuWyboru(int tablica[], int tablica_posortowana[], int* koniec)
+void MenuWyboru(int tablica[], int tablica_posortowana[], int rozmiar, int* koniec)
 {
   int wybor = 0;
   printf("1.Losuj tablice\n"
@@ -78,28 +78,28 @@ void MenuWyboru(int tablica[], int tablica_posortowana[], int* koniec)
   {
     case 1:
     {
-      Losuj(tablica);
+      Losuj(tablica, rozmiar);
       break;
     }
     case 2:
     {
-      Wyswietl(tablica);
+      Wyswietl(tablica, rozmiar);
       break;
     }
     case 3:
     {
-      Srednia(tablica);
+      Srednia(tablica, rozmiar);
       break;
     }
     case 4:
     {
-      Sortuj(tablica,tablica_posortowana);
+      Sortuj(tablica,tablica_posortowana, rozmiar);
       printf("Mediana liczb: %f\n",(tablica_posortowana[4]+tablica_posortowana[5])/2.0);
       break;
     }
     case 5:
     {
-      MinMax(tablica);
+      MinMax(tablica, rozmiar);
       break;
     }
     case 6:
@@ -115,11 +115,12 @@ int main(void)
   srand(time(NULL));
   int koniec = 1;
   int tablica [10];
-  Losuj(tablica);
+  int rozmiar = sizeof(tablica)/sizeof(tablica[0]);
+  Losuj(tablica, rozmiar);
   int tablica_posortowana [10];
   do
   {
-    MenuWyboru(tablica, tablica_posortowana, &koniec);
+    MenuWyboru(tablica, tablica_posortowana, rozmiar, &koniec);
   }while(koniec);
 
   return 0;
