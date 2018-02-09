2839d8de1757200404941f464ac9c780c7d65de8
NoOneNoBody <35255526+NoOneNoBody@users.noreply.github.com>
2018-02-09 11:54:55 +0100

    Update 5.2_Wyznacznik.c

--- 5.2_Wyznacznik.c
+++ 5.2_Wyznacznik.c
@@ -4,27 +4,27 @@ Liczenie wyznacznika macierzy */
 #include <time.h>
 #include <stdlib.h>
 
-int Wyznacznik_macierzy(int macierz[3][3], int rozmiar)
+int Wyznacznik_macierzy_3na3(int macierz[3][3])
 {
   int wyznacznik = 0;
   int iloczyn = 1;
 
-  for(int i=0;i<rozmiar;i++)
+  for(int i=0;i<3;i++)
   {
     iloczyn = 1;
-    for(int j=0;j<rozmiar;j++)
+    for(int j=0;j<3;j++)
     {
-      iloczyn *= macierz[j][(j+i)%rozmiar];
+      iloczyn *= macierz[j][(j+i)%3];
     }
     wyznacznik += iloczyn;
   }
 
-  for(int i=0;i<rozmiar;i++)
+  for(int i=0;i<3;i++)
   {
     iloczyn = 1;
-    for(int j=0;j<rozmiar;j++)
+    for(int j=0;j<3;j++)
     {
-      iloczyn *= macierz[j][(5-(j+i))%rozmiar];
+      iloczyn *= macierz[j][(5-(j+i))%3];
     }
     wyznacznik -= iloczyn;
   }
@@ -50,7 +50,7 @@ int main(void)
   int macierz[3][3];
   const int rozmiar = 3;
   Losowanie_macierzy(macierz, rozmiar);
-  int wyznacznik = Wyznacznik_macierzy(macierz, rozmiar);
+  int wyznacznik = Wyznacznik_macierzy(macierz);
   printf("Wyznacznik wynosi %d\n",wyznacznik);
   return 0;
 }
